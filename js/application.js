23279398e28b652b1ff62623b51955567652c0e9
Patricia Carbajal <carbajal.patricia@gmail.com>
2014-12-15 18:24:38 -0500

    basically working

--- js/application.js
+++ js/application.js
@@ -1,3 +1,6 @@
 $(document).ready(function() {
 
+  new MarkdownWidget("#source-id", "#live-preview")
+
+
 });
--- js/widget.js
+++ js/widget.js
@@ -1,4 +1,16 @@
-function Game(boardString){
-  this.boardString = boardString || this.getRandomBoardString()
-  this.boardColumns = []
-}
+function MarkdownWidget(sourceId, previewDiv){
+  this.input = $(sourceId);
+  this.output = $(previewDiv);
+  $(this.input).on("keyup", function(event){
+    var userInput = $(event.target).val()
+    var parsedInput = this.parseMarkdown(userInput)
+    $(previewDiv).text(parsedInput)
+  })
+};
+
+
+MarkdownWidget.prototype.parseMarkdown = function(input) {
+  return "PARSED" + this.input
+
+};
+
