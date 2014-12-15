26628287e03f1cb43cbddc5a20ae4b807a213617
Steven G. Harms <steven@devbootcamp.com>
2014-12-15 18:28:33 -0500

    Finish markdownWidget

--- md_converter.js
+++ md_converter.js
@@ -1,6 +1,25 @@
-$(function() {
-  $("#md-src").on("keyup", function() {
-    console.log('saw up');
-  });
-});
+function MarkdownWidget (opts) {
 
+  var _hiddenMDWidget = function(opts) {
+    this.sourceSelector  = opts.sourceSel;
+    this.previewSelector = opts.resultSel;
+
+    $(this.sourceSelector).on("keyup", function(event) {
+      var htmlText = this.markdownTransform($(event.target).val());
+      $(this.previewSelector).html(htmlText);
+    }.bind(this));
+  }
+
+  _hiddenMDWidget.prototype = {
+    markdownTransform: function(markdownText) {
+      return marked(markdownText);
+    }
+  }
+
+  new _hiddenMDWidget(opts)
+}
+
+
+$(function() {
+  MarkdownWidget({ sourceSel: "#md-src", resultSel: "#md-result" })
+});
